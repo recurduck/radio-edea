7599d16b8f554712b1ae048265f4555c507b843b
Recurduck Greco <ilaigreco@gmail.com>
2021-12-31 06:46:54 +0200

    Deploy 1.0.2

--- src/store/store.js
+++ src/store/store.js
@@ -8,11 +8,11 @@ const rootReducer = combineReducers({
 })
 
 
-// export const store = createStore(rootReducer, applyMiddleware(thunk))
+export const store = createStore(rootReducer, applyMiddleware(thunk))
 // window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__();
 // Lets wire up thunk and also redux-dev-tools:
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 // export const store = createStore(rootReducer, applyMiddleware(thunk))
 
 
